11eb728c5df04c1b2b5d7b2ae85671ee6526f03d
Wyatt Olson <wyatt.olson@gmail.com>
2009-05-26 23:16:11 -0600

    Empty Comment

---
+++ src/org/homeunix/thecave/moss/common/ClassLoaderFunctions.java
@@ -0,0 +1,251 @@
+/*
+ * Created on Oct 6, 2006 by wyatt
+ */
+package org.homeunix.thecave.moss.common;
+
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.swing.ImageIcon;
+
+/**
+ * Utility to load resources and objects from a jar file.  Basically a 
+ * wrapper to URLClassLoader, which formats a URL from a given Jar file.
+ * Also includes utilities to read entries from a Jar file.
+ *  
+ * @author wyatt
+ *
+ */
+public class ClassLoaderFunctions extends URLClassLoader {
+
+	/**
+	 * Creates a new object specified by className from the given Jar file.
+	 * Will not return a null object - if object is null, it will throw
+	 * an exception.
+	 * @param jarFile Jar file to load from
+	 * @param className Class name to load
+	 * @return The newly created object
+	 * @throws JarLoaderException
+	 */
+	public static Object getObject(File jarFile, String className) throws JarLoaderException {
+		try {
+			String jarFileURL = "jar:file:" + jarFile.getAbsolutePath() + "!/";
+//			Log.debug("Attempting to load from " + jarFileURL);
+			URL url = new URL(jarFileURL);
+
+			ClassLoaderFunctions jl = new ClassLoaderFunctions(url);
+			return jl.getObject(className);
+		}
+		catch(MalformedURLException mue){
+			throw new JarLoaderException(mue);
+		}
+	}
+
+	/**
+	 * Returns a collection of all the entries in a jar file.  Can be
+	 * used to browse for a particular class, etc.
+	 * @param jarFile Jarfile to browse
+	 * @return
+	 */
+	public static Collection<JarEntry> getAllClasses(File jarFile, String rootClass){
+		Collection<JarEntry> jarEntries = new LinkedList<JarEntry>();
+
+		try{
+			JarFile jar = new JarFile(jarFile);
+			for (Enumeration<JarEntry> e = jar.entries(); e.hasMoreElements();){
+				JarEntry je = e.nextElement();
+				if (!je.isDirectory()
+						&& je.getName().endsWith(".class")
+						&& (rootClass == null || je.getName().startsWith(rootClass))){
+					jarEntries.add(je);
+//					Log.debug("JarLoader.getAllClasses(): added " + je.getName() + " to collection of jar entries.");
+				}
+			}
+		}
+		catch (IOException ioe){
+			Logger.getLogger(ClassLoaderFunctions.class.getName()).log(Level.WARNING, "Error loading resource from jar", ioe);
+		}
+
+		return jarEntries;
+	}
+
+	/**
+	 * Returns the given resource as a stream from the specified jar file.  Useful for
+	 * loading plugins and other objects from different files. 
+	 * @param jarFile
+	 * @param resource
+	 * @return
+	 */
+	public static InputStream getResourceAsStreamFromJar(File jarFile, String resource) {
+		try{
+			String jarFileURL = "jar:file:" + jarFile.getAbsolutePath() + "!/";
+//			Log.info("Attempting to load " + resource + " from " + jarFileURL);
+			URL url = new URL(jarFileURL);
+
+			ClassLoaderFunctions jl = new ClassLoaderFunctions(url);
+			return jl.getResourceAsStream(resource);
+		}
+		catch (MalformedURLException mfue){
+			return null;
+		}
+	}
+
+	/**
+	 * Returns the given resource as a stream.
+	 * @param resource
+	 * @return
+	 */
+	public static InputStream getResourceAsStreamFromClasspath(String resource) {
+		return new Object().getClass().getResourceAsStream(resource);
+	}
+
+	/**
+	 * Gets a URL representing the given resource on the class path.  Use relative 
+	 * paths, from the root of the jar file.  For instance, if you want to load 
+	 * the file example.png, in the folder Resources, use the string "Resources/example.png",
+	 * but not "/Resources/example.png".
+	 * @param resource
+	 * @return
+	 */
+	public static URL getResourceAsUrl(String resource){
+		if (resource != null){
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			if (cl == null) {
+				cl = ClassLoaderFunctions.class.getClassLoader();
+			}
+
+			URL urlResource = cl.getResource(resource);
+			if (urlResource != null) {
+				return urlResource;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Creates a new object from a given class name.  Will not return 
+	 * a null object - if the object is null, it will throw an exception
+	 * @param className
+	 * @return
+	 * @throws JarLoaderException
+	 */
+	private Object getObject(String className) throws JarLoaderException {
+		try{
+			Logger.getLogger(this.getClass().getName()).finest("Trying to load object from class " + className);
+			Class<?> c = this.loadClass(className);
+			Object o = c.newInstance();
+
+			if (o == null)
+				throw new JarLoaderException("Loaded object is null.");
+
+			return o;
+		}
+		catch (IllegalAccessException iae){
+			Logger.getLogger(this.getClass().getName()).finest("Could not instantiate object: " + iae.getMessage());
+		}
+		catch (InstantiationException ie){
+			Logger.getLogger(this.getClass().getName()).finest("Could not instantiate object: " + ie.getMessage());
+		}
+		catch (NoClassDefFoundError ncdfe){
+			throw new JarLoaderException(ncdfe);
+		}
+		catch (Exception e){
+			throw new JarLoaderException(e);
+		}
+
+		return null;
+	}
+
+	public static Class<?> getClass(File jarFile, String className) throws JarLoaderException {
+		try {
+			String jarFileURL = "jar:file:" + jarFile.getAbsolutePath() + "!/";
+			Logger.getLogger(ClassLoaderFunctions.class.getName()).finest("Attempting to load from " + jarFileURL);
+			URL url = new URL(jarFileURL);
+
+			ClassLoaderFunctions jl = new ClassLoaderFunctions(url);
+			return Class.forName(className, true, jl);
+		}
+		catch(ClassNotFoundException ex) {
+			throw new JarLoaderException(ex);
+		}
+		catch(MalformedURLException mue){
+			throw new JarLoaderException(mue);
+		}
+	}
+
+	/**
+	 * Returns an ImageIcon from an image loaded in the current classpath.  We
+	 * first check for files in the current path, then we look in jar
+	 * files.
+	 * 
+	 * @param name The relative path and name of the file.  Don't include the leading slash.
+	 * @return An ImageIcon with the given icon, if available, or an empty one otherwise.
+	 */
+	public static Image getImageFromClasspath(String name){
+		ImageIcon icon;
+
+		ClassLoader cl = Thread.currentThread().getContextClassLoader();
+		if (null == cl) {
+			cl = ClassLoaderFunctions.class.getClassLoader();
+		}
+
+		URL imageResource = null;
+		File f = new File(name);
+		if (f.exists() && !f.isDirectory() && f.canRead()){
+			try {
+				imageResource = f.toURL();
+			}
+			catch (MalformedURLException mue){}
+		}
+
+		if (imageResource == null)
+			imageResource = cl.getResource(name);
+
+		if (imageResource != null) {
+			icon = new ImageIcon(Toolkit.getDefaultToolkit().getImage(imageResource));
+		}
+		else {
+			icon = new ImageIcon();
+		}
+
+		return icon.getImage();
+	}
+
+	private ClassLoaderFunctions(URL url){
+		super(new URL[] { url });
+	}
+
+	/**
+	 * @author wyatt
+	 *
+	 * An exception thrown when loading jars.
+	 */
+	public static class JarLoaderException extends Exception {
+		public static final long serialVersionUID = 0;
+
+		public JarLoaderException(Exception e){
+			super(e);
+		}
+
+		public JarLoaderException(String s){
+			super(s);
+		}
+
+		public JarLoaderException(Error e){
+			super(e);
+		}
+	}
+}
---
+++ src/org/homeunix/thecave/moss/common/ParseCommands.java
@@ -0,0 +1,294 @@
+package org.homeunix.thecave.moss.common;
+import java.text.ParseException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+
+/**
+ * This class makes Unix style command line parsing easy for Java applications.
+ * Please see below for instructions on how to use it.
+ *<p>
+ * For a copy of the LGPL license, please see 
+ *  http://www.gnu.org/copyleft/lesser.html
+ *<p>
+ * Copyright (C) 2005 by Wyatt Olson *<p> * This library is free software; you can redistribute it and/or * modify it under the terms of the GNU Lesser General Public * License as published by the Free Software Foundation; either * version 2.1 of the License, or (at your option) any later version. * <p> * This library is distributed in the hope that it will be useful, * but WITHOUT ANY WARRANTY; without even the implied warranty of * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU * Lesser General Public License for more details. * <p> * You should have received a copy of the GNU Lesser General Public * License along with this library; if not, write to the Free Software * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * To use, simply create a collection of ParseVariable objects of
+ * the correct types, and pass them to the static parse() method.
+ * The return value of this is a ParseResults object, which you can
+ * then extract the values from. 
+ * 
+ * @author wyatt
+ */
+public class ParseCommands {
+	
+	
+	/**
+	 * Parses command line arguments, and returns them as a mapping of flag (-x) to variable.  
+	 * The input variables within flagVariables should be Objects (eg, Integer instead of int).
+	 * Currently Boolean, Integer, String, Double , Character, and Float are supported; it is 
+	 * simple to add more as required. 
+	 * @param args Command line argument as given to the main method
+	 * @param help The usage instructions which are shown if the command is type incorrectly.
+	 * @param flags Collection of ParseVariables to define which flags to look for.
+	 * @return
+	 * @throws ParseException
+	 */
+	public static ParseResults parse(String[] args, String help, Collection<ParseVariable> flags) {
+		ParseVariable[] flagArray = flags.toArray(new ParseVariable[1]);
+		return parse(args, help, flagArray);
+	}
+
+	
+	/**
+	 * Parses command line arguments, and returns them as a mapping of flag (-x) to variable.  
+	 * The input variables within flagVariables should be Objects (eg, Integer instead of int).
+	 * Currently Boolean, Integer, String, Double , Character, and Float are supported; it is 
+	 * simple to add more as required. 
+	 * @param args Command line argument as given to the main method
+	 * @param help The usage instructions which are shown if the command is type incorrectly.
+	 * @param flags One or more ParseVariables to define which flags to look for.
+	 * @return
+	 * @throws ParseException
+	 */
+	public static ParseResults parse(String[] args, String help, ParseVariable... flags) {
+		ParseResults results = new ParseResults(flags);
+		
+		//We loop through each string, looking for the __ replacement keyword.
+		// This lets you put spaces in commands without making problems, in 
+		// cases that the shell does not properly do this (appliction launchers, etc).
+		for (int i = 0; i < args.length; i++) {
+			args[i] = args[i].replaceAll("__", " ");
+
+			//Check for Help flag 0 if found, print help and quit.
+			if (args[i].equals("-h")
+					|| args[i].equals("--help")){
+				System.out.println(help);
+				System.exit(0);
+			}
+		}
+		
+		
+		for (int i = 0; i < args.length;){
+			//If this is a flag, we try to parse it
+			if (results.isFlag(args[i])){
+				ParseVariable variable = results.get(args[i]);
+				
+				//Only boolean can be at the end of the list.
+				if (variable.getType().equals(Boolean.class)){
+					results.put(variable.getFlag(), true);
+					i++;
+				}
+				else {
+					if (i + 1 >= args.length
+							|| results.isFlag(args[i + 1])){
+						Logger.getLogger(ParseCommands.class.getName()).warning("Argument expected after flag " + args[i]);
+						System.err.println(help);
+						System.exit(1);
+					}
+					else {
+						i++;
+						try {
+							if (variable.getType().equals(String.class)){
+								results.put(variable.getFlag(), args[i]);
+							}
+							else if (variable.getType().equals(Integer.class)){
+								results.put(variable.getFlag(), Integer.parseInt(args[i]));
+							}
+							else if (variable.getType().equals(Double.class)){
+								results.put(variable.getFlag(), Double.parseDouble(args[i]));
+							}
+							else if (variable.getType().equals(Character.class)){
+								results.put(variable.getFlag(), args[i].toCharArray()[0]);
+							}
+							else if (variable.getType().equals(Float.class)){
+								results.put(variable.getFlag(), Float.parseFloat(args[i]));
+							}
+						}
+						catch (NumberFormatException nfe){
+							Logger.getLogger(ParseCommands.class.getName()).log(Level.WARNING, "Problem parsing number", nfe);
+							results.put(variable.getFlag(), null);
+						}
+						
+						i++;
+					}
+				}
+			}
+			//Otherwise, we assume it is a command.
+			else {
+				results.addCommand(args[i]);
+				i++;
+			}
+		}
+		
+		
+		//Check that all required flags are included:
+		boolean requiredFlagsNotPresent = false;
+		for (ParseVariable variable : results.getVariables()) {
+			if (variable.isRequired() && results.get(variable.getFlag()).getValue() == null){
+				requiredFlagsNotPresent = true;
+				Logger.getLogger(ParseCommands.class.getName()).warning("Flag '" + variable.getFlag() + "' is required, but not present.");
+			}
+		}
+		
+		//If we are missing some required flags, exit.
+		if (requiredFlagsNotPresent){
+			System.err.println(help);
+			System.exit(1);
+		}
+		
+		return results;
+	}
+	
+//	public static class ParseException extends Exception{
+//		static final long serialVersionUID = 0;
+//		
+//		ParseException(Exception e){
+//			super(e);
+//		}
+//		
+//		ParseException(String s){
+//			super(s);
+//		}
+//
+//	}
+	
+	public static class ParseVariable implements Comparable<ParseVariable>{
+		private final String flag;
+		private Object value;
+		private final Class<?> type;
+		private final boolean required;
+		
+		public ParseVariable(String flag, Class<?> type, boolean required) {
+			this.flag = flag;
+			this.type = type;
+			this.required = required;
+		}
+		
+		public String getFlag() {
+			return flag;
+		}
+		public boolean isRequired() {
+			return required;
+		}
+		public void setValue(Object value){
+			this.value = value;
+		}
+		public Object getValue() {
+			return value;
+		}
+		public Class<?> getType(){
+			return type;
+		}
+		public int compareTo(ParseVariable arg0) {
+			return getFlag().compareTo(arg0.getFlag());
+		}
+		@Override
+		public boolean equals(Object arg0) {
+			if (arg0 instanceof ParseVariable)
+				return this.compareTo((ParseVariable) arg0) == 0;
+			else
+				return false;
+		}
+	}
+	
+	public static class ParseResults {
+		private final List<String> commands = new LinkedList<String>();;
+		private final Map<String, ParseVariable> parseVariables = new HashMap<String, ParseVariable>();
+		
+		private ParseResults(ParseVariable... parseVariableList) {
+			for (ParseVariable variable : parseVariableList) {
+				parseVariables.put(variable.getFlag(), variable);
+			}
+		}
+		
+		private void addCommand(String command){
+			commands.add(command);
+		}
+		
+		private boolean isFlag(String flag){
+			return parseVariables.get(flag) != null;
+		}
+		
+		private void put(String flag, Object value){
+			if (value.getClass().equals(parseVariables.get(flag).getType()))
+				parseVariables.get(flag).setValue(value);
+			else 
+				System.out.println("ParseResults.put(): Not putting correct type");
+		}
+		
+		private ParseVariable get(String flag){
+			return parseVariables.get(flag);
+		}
+		
+		public Boolean getBoolean(String flag){
+			if (parseVariables.get(flag).getType().equals(Boolean.class))
+				return (Boolean) parseVariables.get(flag).getValue();
+		
+			return null;
+		}
+
+		public String getString(String flag){
+			if (parseVariables.get(flag).getType().equals(String.class))
+				return (String) parseVariables.get(flag).getValue();
+		
+			return null;
+		}
+
+		public Double getDouble(String flag){
+			if (parseVariables.get(flag).getType().equals(Double.class))
+				return (Double) parseVariables.get(flag).getValue();
+		
+			return null;
+		}
+
+		public Float getFloat(String flag){
+			if (parseVariables.get(flag).getType().equals(Float.class))
+				return (Float) parseVariables.get(flag).getValue();
+		
+			return null;
+		}
+
+		public Character getCharacter(String flag){
+			if (parseVariables.get(flag).getType().equals(Character.class))
+				return (Character) parseVariables.get(flag).getValue();
+		
+			return null;
+		}
+
+		public Integer getInteger(String flag){
+			if (parseVariables.get(flag).getType().equals(Integer.class))
+				return (Integer) parseVariables.get(flag).getValue();
+		
+			return null;
+		}
+		
+		private Collection<ParseVariable> getVariables(){
+			return parseVariables.values();
+		}
+		
+		public List<String> getCommands(){
+			return commands;
+		}
+	}
+	
+	/**
+	 * Simple method to find the index in an array which matches a specified string.
+	 * @param str
+	 * @param array
+	 * @return
+	 */
+/*	private static int matchesArray(String str, Object[] array){
+		for (int i = 0; i < array.length; i++){
+			if (str.matches((String) array[i]))
+				return i;
+		}
+		return -1;
+	}
+*/	
+}
